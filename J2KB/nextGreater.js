a798b72cb31a4d3ff79ccb039f69b7cd5b263067
HongBeen <redbean-2@naver.com>
2021-10-31 17:53:23 +0900

    J2KB 폴더생성, 유닛테스팅 환경, nextGreater.js 풀이

---
+++ J2KB/nextGreater.js
@@ -0,0 +1,33 @@
+"use strict";
+
+unitTesting(
+	(input) => solution(input),
+	[5, 6, 6, 6, 7, -1, -1],
+	[2, 5, -3, -4, 6, 7, 2]
+);
+
+function solution(input) {
+	const stack = [];
+	const result = [];
+	for (let i = 0; i < input.length; i++) {
+		if (i === 0) {
+			stack.push(i);
+			continue;
+		}
+		while (stack.length > 0) {
+			if (input[stack[stack.length - 1]] < input[i]) {
+				result[stack.pop()] = input[i];
+			} else {
+				break;
+			}
+		}
+		stack.push(i);
+	}
+
+	while (stack.length > 0) {
+		const index = stack.pop();
+		result[index] = -1;
+	}
+
+	return result;
+}
--- app.js
+++ app.js
@@ -1,61 +1,26 @@
-'use strict';
-const fs=require('fs');
-const filePath=process.platform==='linux'?'/dev/stdin':'input.txt';
-const input=fs.readFileSync(filePath).toString().split(" ");
-const n=Number(input[0]);
-const k=Number(input[1]);
+"use strict";
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+// const n = Number(fs.readFileSync(filePath).toString().trim());
 
-class Node{
-  constructor(data,next=null){
-    this.data=data;
-    this.next=next;
-  }
-}
-class CircularLinkedList{
-  constructor(){
-    this.tail=null;
-    this.size=0;
-  }
+unitTesting(
+	(input) => solution(input),
+	[5, 6, 6, 6, 7, -1, -1],
+	[2, 5, -3, -4, 6, 7, 2]
+);
 
-  insertTail(data){
-    const newNode=new Node(data);
-
-    if(this.tail === null){
-      this.tail=newNode;
-      this.tail.next=newNode;
-    }else{
-      newNode.next=this.tail.next;
-      this.tail.next=newNode;
-      this.tail=newNode;
-    }
-    this.size+=1;
-    
-    return;
-  }
-
-  removeInFrontOfTail(k){
-    let previousNode,removedNode=this.tail;
-    let removedData;
-
-    for(let i=0;i<k;i++){
-      previousNode=removedNode;
-      removedNode=removedNode.next;
-    }
-    removedData=removedNode.data;
-    previousNode.next=removedNode.next;
-    this.tail=removedNode;
-    this.size-=1;
-
-    return removedData;
-  }
+function unitTesting(func, expected, input) {
+	const result = func(input);
+	if (JSON.stringify(result) === JSON.stringify(expected))
+		console.log(
+			`PASSED, the output of the function is ${result} === ${expected}`
+		);
+	else
+		console.log(
+			`FAILED, the output of the function is ${result} !== ${expected}`
+		);
 }
 
-const cLinkedList=new CircularLinkedList();
-let answer='';
-for(let i=1;i<=n;i++){
-  cLinkedList.insertTail(i);
+function solution(input) {
+	return result;
 }
-while(cLinkedList.size>0){
-  answer+=cLinkedList.removeInFrontOfTail(k)+', ';
-}
-console.log(`<${answer.slice(0,-2)}>`);

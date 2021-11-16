3f44a6339ce19d79e6d1230df224e8c899f732de
HongBeen <redbean-2@naver.com>
2021-11-17 03:13:12 +0900

    leetcode 210

--- J2KB/2주차/3-l210.js
+++ J2KB/2주차/3-l210.js
@@ -1,58 +1,90 @@
-function topologicalSort(arr) {
-	if (!arr.length) return [];
+var findOrder = function (numCourses, pre) {
+	if (pre.length === 0) {
+		const result = [];
+		for (let c = 0; c < numCourses; c++) result.push(c);
+		return result;
+	}
 
-	const map = new Map();
+	const map = topologicalSort(pre);
+	if (map === -1) return [];
 
-	for (let i = 0; i < arr.length; i++) {
-		let [to, from] = arr[i];
+	const sources = findSource(map);
+	if (sources === -1) return [];
 
-		if (map.has(from)) {
-			map.set(from, { ...map.get(from), dest: [...map.get(from).dest, to] });
-		} else {
-			map.set(from, {
-				incoming: 0,
-				dest: [to],
-			});
+	return bfs(map, sources, numCourses);
+};
+
+function bfs(map, sources, num) {
+	const queue = [...sources];
+	const result = [];
+	const courses = new Array(num).fill(0);
+
+	while (queue.length > 0) {
+		const curKey = queue.shift();
+		const { incoming, next } = map.get(curKey);
+
+		if (incoming === 0) {
+			result.push(curKey);
+			courses[curKey] = 1;
 		}
 
-		if (map.has(to)) {
-			map.set(to, { ...map.get(to), incoming: map.get(to).incoming + 1 });
-		} else {
-			map.set(to, {
-				incoming: 1,
-				dest: [],
-			});
+		for (let n of next) {
+			if (map.get(n).incoming === 0) return [];
+			if (--map.get(n).incoming === 0) queue.push(n);
 		}
 	}
-	console.log(map);
 
-	return BFS(map);
-}
+	for (let [key, value] of map) {
+		if (value.incoming > 0) return [];
+	}
 
-function BFS(map) {
-	const result = [];
-	const queue = findTheSource(map);
-
-	while (queue.length) {
-		const curKey = queue.shift();
-		const { incoming, dest } = map.get(curKey);
-
-		if (incoming === 0) result.push(curKey);
-
-		for (let d = dest.length - 1; d >= 0; d--) {
-			const child = dest[d];
-			if (--map.get(child).incoming === 0) queue.push(child);
+	for (let c = 0; c < num; c++) {
+		if (courses[c] === 0) {
+			result.push(c);
 		}
 	}
 
 	return result;
 }
+function findSource(map) {
+	const sourses = [];
 
-function findTheSource(map) {
 	for (let [key, value] of map) {
-		if (value.incoming === 0) return [key];
+		if (value.incoming === 0) {
+			sourses.push(key);
+		}
 	}
-	return []; //cycled tree -> can not finish the course.
+	if (sourses.length > 0) return sourses;
+	// if no incoming with value 0, false;
+	return -1;
+}
+
+function topologicalSort(pre) {
+	const map = new Map();
+
+	for (let [to, from] of pre) {
+		if (to === from) return -1;
+
+		if (!map.has(to)) {
+			map.set(to, {
+				incoming: 1,
+				next: [],
+			}); // 3
+		} else {
+			map.get(to).incoming++;
+		}
+
+		if (!map.has(from)) {
+			map.set(from, {
+				incoming: 0,
+				next: [to],
+			});
+		} else {
+			map.get(from).next.push(to);
+		}
+	}
+
+	return map;
 }
 
 const arr = [
@@ -66,4 +98,4 @@ const arr = [
 	[2, 1],
 	[1, 2],
 ];
-console.log(topologicalSort(arr));
+console.log(findOrder(arr));

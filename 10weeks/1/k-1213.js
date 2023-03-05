d21dad3e59184961620a2c9ee827f8c436f61bcd
HongBeen <redbean-2@naver.com>
2023-03-06 01:24:16 +0900

    update k-1213.js by the lecture way

--- 10weeks/1/k-1213.js
+++ 10weeks/1/k-1213.js
@@ -2,56 +2,40 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const inputs = fs.readFileSync(filePath).toString().trim().split("\n");
 
+// 펠린드롬에서 놓친 것: 길이가 홀수든 짝수든 홀수개 알파벳이 2개이상이면 안됨...
+// 즉, 홀수 알파벳이 2개 이상인지만 체크하면 된다. (<-- 짝수길이라면 홀수개가 등장할 떄, 무조건 2개이상에 걸리므로 여기서 끝난다.)
+// 그리고 홀수개인 알파벳은 가운데에 끼어넣어주면 된다.
+
 function solution(input) {
-	// 1. set up map
 	const map = {};
 	Array.from(input).forEach((v) => {
 		if (map[v]) map[v]++;
 		else map[v] = 1;
 	});
 
-	// 2-for even length
-	if (!(input.length & 1)) {
-		const ans = Array(input.length);
-		let i = 0;
-		while (i < ans.length / 2) {
-			for (let [ch, cnt] of Object.entries(map)) {
-				if (cnt % 2 === 1) return false;
+	let mid = "";
+	let ret = "";
+	let flag = 0;
+	const alphasR = Array.from({length: 26}, (_, i) => String.fromCharCode(65 + 25 - i));
 
-				while (map[ch] > 1) {
-					ans[i] = ch;
-					ans[ans.length - i - 1] = ch;
-					map[ch] -= 2;
-					i++;
-				}
-			}
+	// time O(26 * N/2) ===> O(N)... 와웅~~
+	for (let al of alphasR) {
+		if (!map[al]) continue;
+
+		if (map[al] & 1) {
+			if (++flag === 2) break;
+			mid = al;
+			map[al]--;
 		}
 
-		return ans.join("");
-	}
-
-	// 2-for odd length
-	let i = 0;
-	const ans = Array(input.length);
-	while (i < (ans.length - 1) / 2) {
-		let j = i;
-		for (let ch of Object.keys(map)) {
-			while (map[ch] > 1) {
-				ans[i] = ch;
-				ans[ans.length - i - 1] = ch;
-				map[ch] -= 2;
-				i++;
-			}
+		for (let j = 0; j < map[al]; j += 2) {
+			ret = al + ret + al;
 		}
-		if (j === i) return false;
 	}
 
-	// map에 1개이상인 것들이 딱 한개면 가운데에 넣고 끝.
-	const rest = Object.entries(map).filter(([ch, cnt]) => cnt >= 1);
-	if (rest.length > 1) return false;
-	ans[i] = rest[0][0];
-
-	return ans.join("");
+	if (flag === 2) return "I'm Sorry Hansoo";
+	if (mid) ret = ret.slice(0, ret.length / 2) + mid + ret.slice(ret.length / 2);
+	return ret;
 }
 
-console.log(solution(Array.from(inputs[0]).sort().join("")) || "I'm Sorry Hansoo");
+console.log(solution(Array.from(inputs[0]).join("")) || "I'm Sorry Hansoo");
